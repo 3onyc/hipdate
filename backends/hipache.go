db616cace9956499919adb7d6717d42be6de297e
3onyc <3onyc@x3tech.com>
2014-07-27 02:28:58 +0200

    Move NewHipacheBackend next to struct

--- backends/hipache.go
+++ backends/hipache.go
@@ -10,6 +10,12 @@ type HipacheBackend struct {
 	r redis.Conn
 }
 
+func NewHipacheBackend(r redis.Conn) *HipacheBackend {
+	return &HipacheBackend{
+		r: r,
+	}
+}
+
 func (hb *HipacheBackend) AddUpstream(
 	h hipdate.Host,
 	u hipdate.Upstream,
@@ -76,10 +82,4 @@ func (hb *HipacheBackend) hostClear(h hipdate.Host) error {
 
 	log.Println("Initialised", h)
 	return nil
-}
-
-func NewHipacheBackend(r redis.Conn) *HipacheBackend {
-	return &HipacheBackend{
-		r: r,
-	}
 }
