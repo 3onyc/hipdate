e387daed8cb61287d32bbbb5f84e46132c072193
3onyc <3onyc@x3tech.com>
2014-07-31 14:56:29 +0200

    Split up initialising sources and backends from main() func

--- hipdated/main.go
+++ hipdated/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/3onyc/hipdate/backends"
 	"github.com/3onyc/hipdate/shared"
@@ -10,6 +11,10 @@ import (
 	"sync"
 
 	_ "github.com/3onyc/hipdate"
+)
+
+var (
+	BackendNotFoundError = errors.New("Backend not found")
 )
 
 type HostPortPair struct {
@@ -25,45 +30,67 @@ func main() {
 	cfg := ConfigParseEnv(os.Environ())
 
 	if cfg.Backend == "" {
-		log.Fatalln("No backend selected")
+		log.Fatalln("[FATAL] No backend selected")
 	}
 
 	if len(cfg.Sources) == 0 {
-		log.Fatalln("No sources selected")
+		log.Fatalln("[FATAL] No sources selected")
 	}
 
 	wg := &sync.WaitGroup{}
 	ce := make(chan *shared.ChangeEvent)
 
-	backendInitFn, ok := backends.BackendMap[cfg.Backend]
-	if !ok {
-		log.Fatalf("ERR: Backend %s not found\n", cfg.Backend)
-	}
-
-	be, err := backendInitFn(cfg.Options)
-	if err != nil {
-		log.Fatalln("ERR:", err)
-	}
-
-	srcs := []sources.Source{}
-	for _, sn := range cfg.Sources {
-		srcInitFn, ok := sources.SourceMap[sn]
-		if !ok {
-			log.Fatalf("ERR: Source %s not found\n", sn)
-			continue
-		}
-
-		src, err := srcInitFn(cfg.Options, ce, wg)
-		if err != nil {
-			log.Fatalln("ERR", err)
-			continue
-		}
-
-		srcs = append(srcs, src)
+	srcs := InitSources(cfg, ce, wg)
+	be, err := InitBackend(cfg)
+	switch {
+	case err == BackendNotFoundError:
+		log.Fatalf("[FATAL] Backend '%s' not found\n", cfg.Backend)
+	case err != nil:
+		log.Fatalf("[FATAL][backend:%s] %s", cfg.Backend, err)
 	}
 
 	app := NewApplication(be, srcs, ce, wg)
 
 	log.Println("Starting...")
 	app.Start()
+}
+
+func InitSources(
+	cfg Config,
+	ce chan *shared.ChangeEvent,
+	wg *sync.WaitGroup,
+) []sources.Source {
+	srcs := []sources.Source{}
+
+	for _, sn := range cfg.Sources {
+		srcInitFn, ok := sources.SourceMap[sn]
+		if !ok {
+			log.Printf("[SEVERE] Source '%s' not found\n", sn)
+			continue
+		}
+
+		src, err := srcInitFn(cfg.Options, ce, wg)
+		if err != nil {
+			log.Printf("[SEVERE][source:%s] %s", sn, err)
+			continue
+		}
+
+		srcs = append(srcs, src)
+	}
+
+	return srcs
+}
+
+func InitBackend(cfg Config) (backends.Backend, error) {
+	backendInitFn, ok := backends.BackendMap[cfg.Backend]
+	if !ok {
+		return nil, BackendNotFoundError
+	}
+
+	be, err := backendInitFn(cfg.Options)
+	if err != nil {
+		return nil, err
+	}
+
+	return be, nil
 }
