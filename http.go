82bb31ba4e808cf67e2d0e8b42efbfd11fda1459
3onyc <3onyc@x3tech.com>
2014-08-18 19:42:18 +0200

    Switch stoppableListener back to upstream repository

--- http.go
+++ http.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/3onyc/hipdate/backends"
-	"github.com/3onyc/stoppableListener"
+	"github.com/hydrogen18/stoppableListener"
 	"log"
 	"net"
 	"net/http"
